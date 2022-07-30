a5c6aedf674a9812fc433c62a96ffb091560ac4f
KateBlazhko <kateugnicheva@gmail.com>
2022-07-30 14:29:07 +0300

    refactor: correct goods.ts as noted

--- online-store/src/components/view/goods.ts
+++ online-store/src/components/view/goods.ts
@@ -2,10 +2,15 @@ import Control from "../common/control";
 import { Card } from "./card";
 import { IDataItem } from "../model/appModel";
 
+enum InnerText {
+  title = "Our goods for you",
+  noFoundedText = "Sorry, nothing found..."
+}
+
 class Goods extends Control {
   private dataList: Card[];
   private cardWrap: Control;
-  private text: Control | null;
+  private noFoundedText: Control;
   private cart: string[];
   private onCartUp: (id: string, count: number) => boolean;
   private onCartDown: (id: string) => void;
@@ -23,14 +28,15 @@ class Goods extends Control {
     this.onCartUp = onCartUp;
     this.onCartDown = onCartDown;
 
-    new Control(this.node, "h2", "title title_goods", "Our goods for you");
+    new Control(this.node, "h2", "title title_goods", InnerText.title);
 
-    this.text = new Control(
-      this.node,
+    this.noFoundedText = new Control(
+      null,
       "p",
       "goods__text",
-      "Sorry, nothing found..."
+      InnerText.noFoundedText
     );
+
     this.cardWrap = new Control(this.node, "div", "goods__card-wrapper");
     this.dataList = [];
   }
@@ -42,21 +48,14 @@ class Goods extends Control {
       this.dataList.map((dataItem) => dataItem.destroy());
 
     if (data && data.length > 0) {
-      this.text && this.text.destroy();
-      this.text = null;
-
+      this.noFoundedText.destroy();
       this.draw(data);
+
     } else {
-      if (!this.text)
-        this.text = new Control(
-          this.node,
-          "p",
-          "goods__text",
-          "Sorry, nothing found..."
-        );
+      this.node.appendChild(this.noFoundedText.node)
     }
   }
-
+  
   private draw(data: readonly IDataItem[]) {
     this.dataList = data.map((dataItem) => {
       const isInCart = this.cart.includes(dataItem.id);
